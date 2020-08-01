d9b41322bf97106b798914b33988782fe1d07834
fateku <68038652+fateku@users.noreply.github.com>
2020-08-01 15:33:19 +0800

    Create rectangle.java

---
+++ rectangle.java
@@ -0,0 +1,12 @@
+public class Star1 {
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++) {
+			for(int b=1;b<=5;b++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+	
+}
